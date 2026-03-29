9e2787b88135cf41187e10dea6c0f538327430a8
Callum Luxford <151152271+Callum-Luxford@users.noreply.github.com>
2026-03-29 15:59:58 +0100

    Push to VPS

--- server/src/app.js
+++ server/src/app.js
@@ -32,7 +32,7 @@ const clientDistPath = path.join(__dirname, "../../client/dist");
 
 app.use(express.static(clientDistPath));
 
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(clientDistPath, "index.html"));
 });
 
