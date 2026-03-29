a353ceaaef922626f441094d4b9bbe5b834e0636
Callum Luxford <151152271+Callum-Luxford@users.noreply.github.com>
2026-03-29 17:30:58 +0100

    .

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
